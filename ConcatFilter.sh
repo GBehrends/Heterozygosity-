f46595799a09d04fe6ce5976c7e264ee7b60c9bf
Garrett Behrends <82614773+GBehrends@users.noreply.github.com>
2022-05-23 14:18:01 -0500

    Update ConcatFilter.sh

--- ConcatFilter.sh
+++ ConcatFilter.sh
@@ -5,24 +5,21 @@
 #SBATCH --partition quanah
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=<# of scaffolds in helper list>
+#SBATCH --array=<# of g.vcf prefixes>
 
 
 		
 							###Beginning Steps###
 
 # define input files from helper file during genotyping
-input_array=$( head -n${SLURM_ARRAY_TASK_ID} <helper list> | tail -n1 )
+input_array=$( head -n${SLURM_ARRAY_TASK_ID} <list of g.vcf file prefixes> | tail -n1 )
 
 #Begin for loop for all species directories 
 for i in $(cat ../specieslist); do 
 
-# define main working directory
-<working_directory>
-
 # Run vcftools 
-vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 0.5 --minQ 20 --minGQ 20 --minDP 6 --max-meanDP 50  --max-alleles 2 --mac 1 --max-maf 0.49 --remove-indels --include-non-variant-sites --recode --recode-INFO-all --out ${i}/04_vcf_all_sites/${input_array}
+vcftools --vcf <location of g.vcf files> --max-missing 0.5 --minQ 20 --minGQ 20 --minDP 6 --max-meanDP 50  --max-alleles 2 --mac 1 --max-maf 0.49 --remove-indels --include-non-variant-sites --recode --recode-INFO-all --out Het/VCF 
 
 # Run bcftools to simplify vcfs for Het calculation 
-bcftools query -f '%POS\t%REF\t%ALT[\t%GT]\n ' ${i}/04_vcf_all_sites/${input_array}.recode.vcf > ${i}/04_vcf_all_sites/${input_array}.simple.vcf;
+bcftools query -f '%POS\t%REF\t%ALT[\t%GT]\n ' Het/VCF/${input_array}.vcf > Het/SimpleVCF/${input_array}.simple.vcf;
 done 
